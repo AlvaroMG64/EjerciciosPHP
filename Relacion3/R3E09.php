dc66b7392aefb8a105e42754681aa1c36e00c91c
AlvaroMG64 <ciencias8000@gmail.com>
2025-10-29 11:38:50 +0100

    Añadidos ejercicios del 9 al 11

--- Relacion3/R3E09.php
+++ Relacion3/R3E09.php
@@ -8,6 +8,18 @@
 <body class="bg-light">
     <div class="container mt-5">
         <div class="card shadow p-4">
+            <div class="card-body">
+                <h1 class="text-center text-primary mb-4">Extracción de la palabra más larga de un texto</h1>
+                <form method="post" class="row g-3">
+                    <div class="col-md-6 offset-md-3">
+                        <label for="texto" class="form-label">Introduzca un texto:</label>
+                        <input type="text" class="form-control" id="texto" name="texto" required>
+                    </div>
+                    <div class="col-12 text-center">
+                        <button type="submit" class="btn btn-success mt-3">Enviar</button>
+                    </div>
+                </form>
+            </div>
             
             <h1 class="text-center text-primary mb-4">-</h1>
 
